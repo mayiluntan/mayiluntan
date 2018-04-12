4d95c1b905ce476decad9361ede2b6e6d874763f
zfyuser <23467668@qq.com>
2018-04-12 19:37:12 +0800

    蚂蚁圈

--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -4,8 +4,18 @@ const app = getApp()
 
 Page({
   data: {
+    menuSelected:0
   },
   onLoad: function () {
 
+  },
+  menuClicked: function (event) {
+    var menutype = event.currentTarget.dataset.menutype;
+    if (this.data.menuSelected == menutype) {
+      return;
+    }
+    this.setData({
+      menuSelected: menutype
+    })
   }
 })
