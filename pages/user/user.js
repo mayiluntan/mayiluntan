e0b6176d23f1777d37924eaa147d42e3077d4fda
zfyuser <23467668@qq.com>
2018-04-18 15:57:01 +0800

    1

--- pages/user/user.js
+++ pages/user/user.js
@@ -2,8 +2,24 @@ const app = getApp()
 
 Page({
   data: {
+    list:[
+      { name: '个人主页', page: '' },
+      { name: '帖子管理', page: '' },
+      { name: '动态管理', page: '' },
+      { name: '关注的人', page: '' },
+      { name: '我的收藏', page: '' },
+      { name: '关于我们', page: '/pages/user/about/about' },
+    ]
   },
   onLoad: function () {
 
+  },
+  listClick(e){
+    var page = e.currentTarget.dataset.page
+    if (page!=''){
+      wx.navigateTo({
+        url: page,
+      })
+    }
   }
 })
