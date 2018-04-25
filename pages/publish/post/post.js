ced4dec4d745a30034cf5047404fbb62041ffe29
xuchunyu <342161304@qq.com>
2018-04-25 17:11:01 +0800

    sync

--- pages/publish/post/post.js
+++ pages/publish/post/post.js
@@ -6,11 +6,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    pics:[],
-    picCount:0,
-    indexArray:[0,0],
-    cateArray:[['首页','黄页'], ['房屋出租', '房屋求租', '二手市场', '求职招聘', '汽车交易', '求助问事', '拼车信息', '短租民宿', '生意转让', '交友项目', '宠物相关', '二手教材', '二手房产', '同城交友', '家居家具', '数码电子']],
-    on:1
+    pics: [],
+    picCount: 0,
+    indexArray: [0, 0],
+    cateArray: [['房屋出租', '房屋求租', '二手市场', '求职招聘', '汽车交易', '求助问事', '拼车信息', '短租民宿', '生意转让', '交友项目', '宠物相关', '二手教材', '二手房产', '同城交友', '家居家具', '数码电子'], ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']],
+    on: 1
   },
 
   /**
@@ -41,37 +41,51 @@ Page({
       }
     })
   },
-  cateChange(e){
+  cateChange(e) {
     //console.log(e)
-    var v=e.detail.value;
-    if(this.data.index!=v){
+    var v = e.detail.value;
+    if (this.data.index != v) {
       this.setData({
-        index:v
+        index: v
       })
-    } 
+    }
   },
   columnChange: function (e) {
-    if (e.detail.column==1){
+    if (e.detail.column == 1) {
       return;
     }
     var cateArray = this.data.cateArray
     switch (e.detail.value) {
-      case 0: cateArray[1] = ['房屋出租', '房屋求租', '二手市场', '求职招聘', '汽车交易', '求助问事', '拼车信息', '短租民宿', '生意转让', '交友项目', '宠物相关', '二手教材', '二手房产', '同城交友', '家居家具', '数码电子'];break;
-      case 1: cateArray[1] = ['cate1', 'cate2', 'cate3'];break;
+      case 0: cateArray[1] = ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']; break;
+      case 1: cateArray[1] = ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']; break;
+      case 2: cateArray[1] = ['家居家具', '数码电子', '二手教材', '宠物相关', '服装饰品', '游戏娱乐', '美容护肤', '食品饮料', '宝宝用品', '其它综合']; break;
+      case 3: cateArray[1] = ['求职招聘']; break;
+      case 4: cateArray[1] = ['汽车交易']; break;
+      case 5: cateArray[1] = ['求助问事']; break;
+      case 6: cateArray[1] = ['人找车', '车找人']; break;
+      case 7: cateArray[1] = ['短租民宿']; break;
+      case 8: cateArray[1] = ['生意转让']; break;
+      case 9: cateArray[1] = ['交友项目']; break;
+      case 10: cateArray[1] = ['宠物相关']; break;
+      case 11: cateArray[1] = ['二手教材']; break;
+      case 12: cateArray[1] = ['二手房产']; break;
+      case 13: cateArray[1] = ['同城交友']; break;
+      case 14: cateArray[1] = ['家居家具']; break;
+      case 15: cateArray[1] = ['数码电子']; break;
     }
     this.setData({
-      cateArray:cateArray
+      cateArray: cateArray
     })
   },
-  clickSwitch(e){
-    var v=this.data.on;
-    if(v==1){
-      v=0
-    }else{
-      v=1
+  clickSwitch(e) {
+    var v = this.data.on;
+    if (v == 1) {
+      v = 0
+    } else {
+      v = 1
     }
     this.setData({
-      on:v
+      on: v
     })
   }
 })
