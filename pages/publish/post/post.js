ce9390021478e9df189483eb31662391306ae1d0
xuchunyu <342161304@qq.com>
2018-05-11 13:18:42 +0800

    sync

--- pages/publish/post/post.js
+++ pages/publish/post/post.js
@@ -11,7 +11,7 @@ Page({
     picIds:[],
     picCount:0,
     indexArray: [0, 0],
-    cateArray: [['房屋出租', '房屋求租', '二手市场', '求职招聘', '汽车交易', '求助问事', '拼车信息', '短租民宿', '生意转让', '交友项目', '宠物相关', '二手教材', '二手房产', '同城交友', '家居家具', '数码电子'], ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']],
+    cateArray: [['房屋信息',  '二手市场', '求职招聘', '汽车交易', '求助问事', '拼车信息', '短租民宿', '生意转让', '交友项目', '宠物相关', '二手教材', '房产信息', '同城交友', '家居家具', '数码电子'], ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']],
     dayArray:['1天','7天','30天'],
     dayIndex:0,
     postData:{
@@ -121,22 +121,21 @@ Page({
     }
     var cateArray = this.data.cateArray
     switch (e.detail.value) {
-      case 0: cateArray[1] = ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']; break;
-      case 1: cateArray[1] = ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay']; break;
-      case 2: cateArray[1] = ['家居家具', '数码电子', '二手教材', '宠物相关', '服装饰品', '游戏娱乐', '美容护肤', '食品饮料', '宝宝用品', '其它综合']; break;
-      case 3: cateArray[1] = ['求职招聘']; break;
-      case 4: cateArray[1] = ['汽车交易']; break;
-      case 5: cateArray[1] = ['求助问事']; break;
-      case 6: cateArray[1] = ['人找车', '车找人']; break;
-      case 7: cateArray[1] = ['短租民宿']; break;
-      case 8: cateArray[1] = ['生意转让']; break;
-      case 9: cateArray[1] = ['交友项目']; break;
-      case 10: cateArray[1] = ['宠物相关']; break;
-      case 11: cateArray[1] = ['二手教材']; break;
-      case 12: cateArray[1] = ['二手房产']; break;
-      case 13: cateArray[1] = ['同城交友']; break;
-      case 14: cateArray[1] = ['家居家具']; break;
-      case 15: cateArray[1] = ['数码电子']; break;
+      case 0: cateArray[1] = ['出租/合租', '短租民宿', '办公/商铺', '仓库/车位', 'Homestay',"求租"]; break;
+      case 1: cateArray[1] = ['家居家具', '数码电子', '二手教材', '宠物相关', '服装饰品', '游戏娱乐', '美容护肤', '食品饮料', '宝宝用品', '其它综合']; break;
+      case 2: cateArray[1] = ['求职招聘']; break;
+      case 3: cateArray[1] = ['汽车交易']; break;
+      case 4: cateArray[1] = ['求助问事']; break;
+      case 5: cateArray[1] = ['人找车', '车找人']; break;
+      case 6: cateArray[1] = ['短租民宿']; break;
+      case 7: cateArray[1] = ['生意转让']; break;
+      case 8: cateArray[1] = ['交友项目']; break;
+      case 9: cateArray[1] = ['宠物相关']; break;
+      case 10: cateArray[1] = ['二手教材']; break;
+      case 11: cateArray[1] = ['二手房产']; break;
+      case 12: cateArray[1] = ['同城交友']; break;
+      case 13: cateArray[1] = ['家居家具']; break;
+      case 14: cateArray[1] = ['数码电子']; break;
     }
     this.setData({
       cateArray: cateArray
@@ -352,6 +351,11 @@ Page({
     postData.wechat = e.detail.value;
     this.setData({
       postData: postData
+    })
+  },
+  selectArea() {
+    wx.navigateTo({
+      url: '/pages/areaSelect/areaSelect',
     })
   }
 })
