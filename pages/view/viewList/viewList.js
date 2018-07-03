e18e8afd348e83666b8b1374cf446b70c72d7eba
zfyuser <23467668@qq.com>
2018-07-03 10:55:36 +0800

    1

--- pages/view/viewList/viewList.js
+++ pages/view/viewList/viewList.js
@@ -44,7 +44,7 @@ Page({
     var v = cate-1;
     var cateArray = ['全部分类']
     switch (v) {
-      case 0: cateArray = ['全部分类','找房', '招租']; break;
+      case 0: cateArray = ['全部分类','求组', '招租']; break;
       case 1: cateArray = ['全部分类','家居家具', '数码电子', '二手教材', '宠物相关', '服装饰品', '游戏娱乐', '美容护肤', '食品饮料', '宝宝用品', '其它综合']; break;
       case 2: cateArray = ['全部分类','求职', '招聘']; break;
       case 3: cateArray = ['全部分类','求购', '出售']; break;
